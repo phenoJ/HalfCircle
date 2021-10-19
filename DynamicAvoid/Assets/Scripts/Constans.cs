fd813dee65cba5d67cdf3ea8a85fb2345d16e4e3
CurRabbit <hazin.lu@gmail.com>
2021-10-19 11:19:13 +0800

    删除map

---
+++ DynamicAvoid/Assets/Scripts/Constans.cs
@@ -0,0 +1,17 @@
+﻿
+
+public class Constans
+{
+    public const int MAX_ROW_CNT = 40;
+    public const int MAX_COL_CNT = 40;
+    public const int GRID_WIDTH = 20;
+    public const int GRID_HEIGHT = 20;
+
+    public enum DirType
+    {
+        Top,
+        Bottom,
+        Left,
+        Right
+    }
+}
